4623fc8b5c852bce962d8e009eeeac20d6b0e95e
Carlos Alberto Ruiz Naranjo <carlosruiznaranjo@gmail.com>
2025-02-24 11:26:01 +0100

    tb sincos

--- tb/sincos/sincos_tb.vhd
+++ tb/sincos/sincos_tb.vhd
@@ -21,9 +21,9 @@ end entity sincos_tb;
 
 architecture bench of sincos_tb is
     -- Clock period
-    constant CLK_PERIOD : time := 5 ns;
+    constant CLK_PERIOD     : time     := 5 ns;
     constant cnt_DATA_WIDTH : positive := 16;
-    
+
     -- Generics
     -- Ports
     signal aclk                : std_logic := '0';
@@ -31,7 +31,15 @@ architecture bench of sincos_tb is
     signal s_axis_phase_tdata  : std_logic_vector(15 downto 0);
     signal m_axis_dout_tvalid  : std_logic;
     signal m_axis_dout_tdata   : std_logic_vector(31 downto 0);
+
+    signal sin_data : std_logic_vector(cnt_DATA_WIDTH - 1 downto 0) := (others => '0');
+    signal cos_data : std_logic_vector(cnt_DATA_WIDTH - 1 downto 0) := (others => '0');
+
+    signal finish_test : boolean := false;
 begin
+
+    sin_data <= m_axis_dout_tdata(cnt_DATA_WIDTH - 1 downto 0);
+    cos_data <= m_axis_dout_tdata(cnt_DATA_WIDTH * 2 - 1 downto cnt_DATA_WIDTH);
 
     sincos_inst : entity dsp.sincos
         port map (
@@ -61,10 +69,33 @@ begin
                 end loop;
                 s_axis_phase_tvalid <= '0';
 
+                wait for 100 * CLK_PERIOD;
+                finish_test <= true;
+                wait until rising_edge(aclk);
+
                 test_runner_cleanup(runner);
             end if;
         end loop;
     end process main;
+
+    process is
+        variable data_output_sin_array : integer_array_t := new_1d(length => 0, bit_width => cnt_DATA_WIDTH, is_signed => true);
+        variable data_output_cos_array : integer_array_t := new_1d(length => 0, bit_width => cnt_DATA_WIDTH, is_signed => true);
+
+    begin
+        if (finish_test = true) then
+            info("Test finished");
+            save_csv(data_output_sin_array, g_TB_PATH & "/" & g_TEST_NAME & "_sin_output.csv");
+            save_csv(data_output_cos_array, g_TB_PATH & "/" & g_TEST_NAME & "_cos_output.csv");
+            wait;
+        else
+            if (m_axis_dout_tvalid = '1') then
+                append(data_output_sin_array, to_integer(signed(sin_data)));
+                append(data_output_cos_array, to_integer(signed(cos_data)));
+            end if;
+        end if;
+        wait until rising_edge(aclk);
+    end process;
 
     aclk <= not aclk after CLK_PERIOD / 2;
 
